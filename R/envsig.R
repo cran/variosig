0f89db82fcb92d0c00e51fcc397ef6dd55db5765
Craig Wang <craig.wang@uzh.ch>
2018-12-20 14:50:02 +0000

    version 0.3

--- R/envsig.R
+++ R/envsig.R
@@ -1,5 +1,4 @@
-envsig <- function(envlist, adjustment = c("holm", "hochberg", "hommel",
-                                            "bonferroni", "BH", "BY", "fdr", "none")){
+envsig <- function(envlist, index = NULL, method = c("eb", "fisher", "min")){
   if (!is.list(envlist)) stop(
     "The method 'envsig' must be applied to an object from the output of envelope()")
   if (is.null(envlist$variogram0)) stop(
@@ -18,10 +17,41 @@ envsig <- function(envlist, adjustment = c("holm", "hochberg", "hommel",
              min(table(envlist$variogram0$v[i] < envlist$variogram[i,]))/dims[2]
            })
          })
+  if (is.null(index)){
+    index <- length(pvals)
+  }
   # minimum p-value is 1/nsim
-  pvals[pvals==1 | pvals==0] <- 1/dims[2]
-  pvals.adj <- stats::p.adjust(pvals, method = adjustment, n = length(pvals))
-  # Fisher's method
-  pvals.all <- pchisq( -2*sum(log(pvals.adj)), df = 2*length(pvals.adj), lower.tail=FALSE)
-  return(list(p.pointwise = pvals.adj, p.overall = pvals.all))
+  pvals[pvals == 0] <- 1/dims[2]
+  # p-value combination method
+  rep <- ncol(envlist$variogram) - 1
+  Fxs <- matrix(NA, nrow = rep, ncol = index)
+  for (i in 1:index){
+    Fxs[,i] <- ecdf(envlist$variogram[i,])(envlist$variogram[i,-1])
+  }
+  del <- unique(unlist(sapply(1:index, function(x) which(Fxs[,x] == 1))))
+  covs <- matrix(NA, nrow = index, ncol = index)
+  for (i in 1:(index-1)){
+    for (j in (i+1):(index)){
+      covs[i,j] <- stats::cov(-2 * log(1- Fxs[-del,i]), -2 * log(1- Fxs[-del,j]))
+    }
+  }
+  pvals <- apply(envlist$variogram, 1, function(x){1 - mean(x[1] < x[-1])})[1:index]
+  pvals[pvals == 0] <- 1/rep
+  switch(method,
+         "eb" = {
+           sum_covs <- sum(covs[upper.tri(covs)])
+           E <- 2 * index
+           Var <- 4 * index + 2*sum_covs
+           f = 2*E^2 / Var
+           c = Var/(2*E)
+           p.overall <- pchisq(-2*sum(log(pvals))/c, df = f, lower.tail = F)
+         },
+         "fisher" = {
+           p.overall <- pchisq(-2*sum(log(pvals)), df = 2*length(pvals), lower.tail = F)
+         },
+         "min" = {
+           p.overall <- min(pvals)
+         }
+  )
+  return(list(p.pointwise = pvals, p.overall = p.overall))
 }
