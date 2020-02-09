5d83aa4d8b48ee5a23aa4459bd47272a96cd21b9
Craig Wang <craig.wang@math.uzh.ch>
2020-02-09 18:20:02 +0000

    version 0.3-1

--- R/envelope.R
+++ R/envelope.R
@@ -49,41 +49,45 @@ envelope.gstatVariogram <- function(vario, data, locations = coordinates(data), 
 envelope.variogram <- function(vario, data, locations = data$coords, trend = NULL,
                                cluster = FALSE, n.cluster = NULL,
                                nsim = 999, conf.level = 0.95, save.sim = FALSE, ...) {
-  dots <- list(...)
-  dataValue <- data$data
-  variogramDefault <- vario
-  if (cluster & is.null(n.cluster)) stop("The number of clusters is not specified.")
-  if (!is.null(trend)){
-    dataValues <- lm(dataValue ~ trend-1)$residuals
-  } else {
-    dataValues <- dataValue
-  }
-  simulation <- list()
+  if (requireNamespace("geoR", quietly = TRUE)) {
+    dots <- list(...)
+    dataValue <- data$data
+    variogramDefault <- vario
+    if (cluster & is.null(n.cluster)) stop("The number of clusters is not specified.")
+    if (!is.null(trend)){
+      dataValues <- lm(dataValue ~ trend-1)$residuals
+    } else {
+      dataValues <- dataValue
+    }
+    simulation <- list()
 
-  if (cluster){
-    simulation$ID <- as.factor(stats::kmeans(locations, centers = n.cluster)$cluster)
-  } else {
-    simulation$ID <- rep(1, length(dataValues))
-  }
+    if (cluster){
+      simulation$ID <- as.factor(stats::kmeans(locations, centers = n.cluster)$cluster)
+    } else {
+      simulation$ID <- rep(1, length(dataValues))
+    }
 
-  dat <- data.frame(dataValue, locations, ID = simulation$ID)
-  dat <- dat[order(dat$ID), ]
+    dat <- data.frame(dataValue, locations, ID = simulation$ID)
+    dat <- dat[order(dat$ID), ]
 
-  simulation$data <- lapply(1:nsim, function(i)
-    do.call(rbind, lapply(split(dat, dat$ID), function(x) x[sample(nrow(x)),])))
+    simulation$data <- lapply(1:nsim, function(i)
+      do.call(rbind, lapply(split(dat, dat$ID), function(x) x[sample(nrow(x)),])))
 
-  simulation$variogram <- cbind(variogramDefault$v, sapply(1:nsim, function(i){
+    simulation$variogram <- cbind(variogramDefault$v, sapply(1:nsim, function(i){
       data.temp <- data.frame(simulation$data[[i]][,c(2:3)], dat[,1])
-      data.temp <- as.geodata(data.temp, coords.col = 1:2, data.col = 3)
+      data.temp <- geoR::as.geodata(data.temp, coords.col = 1:2, data.col = 3)
       geoR::variog(data.temp, messages = FALSE, ...)$v
-  }))
+    }))
 
-  simulation$upper <- apply(simulation$variogram, 1, quantile, probs = 1-(1-conf.level)/2)
-  simulation$lower <- apply(simulation$variogram, 1, quantile, probs = (1-conf.level)/2)
-  simulation$data.values <- dataValues
-  simulation$variogram0 <- variogramDefault
-  simulation$conf.level <- conf.level
-  if (!save.sim){simulation$data <- NULL}
-  return(simulation)
+    simulation$upper <- apply(simulation$variogram, 1, quantile, probs = 1-(1-conf.level)/2)
+    simulation$lower <- apply(simulation$variogram, 1, quantile, probs = (1-conf.level)/2)
+    simulation$data.values <- dataValues
+    simulation$variogram0 <- variogramDefault
+    simulation$conf.level <- conf.level
+    if (!save.sim){simulation$data <- NULL}
+    return(simulation)
+  } else {
+    stop("The 'geoR' package is not installed, please install it first to use the envelope() function with a 'variogram' class.")
+  }
 }
 
--- tests/testthat/test.R
+++ tests/testthat/test.R
@@ -1,7 +1,6 @@
 library(variosig)
 library(gstat)
 library(sp)
-library(geoR)
 library(testthat)
 
 data(meuse)
@@ -18,13 +17,16 @@ expect_that(perm, is_a("list"))
 expect_that(envsig(perm, index = 2, method = "eb"), is_a("list"))
 expect_that(envplot(perm), is_a("character"))
 
-meuse <- as.geodata(obj = meuse, data.col = 4, covar.col = 1:3,
-                    covar.names = c("cadmium", "copper","lead"))
-meuse$data <- log(meuse$data)
-vario2 <- variog(meuse,max.dist=1500)
-perm2 <- envelope(vario2, meuse, nsim=9, cluster = TRUE, n.cluster = 10, max.dist=1500)
-expect_that(perm2, is_a("list"))
-expect_that(envsig(perm2, method = "eb"), is_a("list"))
-expect_that(envplot(perm2), is_a("character"))
+if (requireNamespace("geoR", quietly = TRUE)) {
+  library(geoR)
+  meuse <- as.geodata(obj = meuse, data.col = 4, covar.col = 1:3,
+                      covar.names = c("cadmium", "copper","lead"))
+  meuse$data <- log(meuse$data)
+  vario2 <- variog(meuse,max.dist=1500)
+  perm2 <- envelope(vario2, meuse, nsim=9, cluster = TRUE, n.cluster = 10, max.dist=1500)
+  expect_that(perm2, is_a("list"))
+  expect_that(envsig(perm2, method = "eb"), is_a("list"))
+  expect_that(envplot(perm2), is_a("character"))
+}
 
 
